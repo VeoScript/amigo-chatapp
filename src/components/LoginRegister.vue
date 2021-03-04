99d747899ebf58907cdbe3ad92fef5d47829e988
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-04 15:13:30 +0800

    added login form - name, email and password

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -1,9 +1,33 @@
 <template>
-  <p>{{ tab }}</p>
+  <q-form>
+    <q-input
+      v-model="formData.name"
+      outlined
+      label="Name" />
+    <q-input
+      v-model="formData.email"
+      outlined
+      type="email"
+      label="Email" />
+    <q-input
+      v-model="formData.password"
+      outlined
+      type="password"
+      label="Password" />
+  </q-form>
 </template>
 
 <script>
 export default {
-  props: ['tab']
+  props: ['tab'],
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
