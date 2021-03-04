07a844342f7f5fa6ac5594806c62cdafee7a71a9
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-04 13:22:39 +0800

    added messages data and display it to bubblechat

--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -2,13 +2,11 @@
   <q-page class="flex column justify-end">
     <div class="q-pa-md column col">
       <q-chat-message
-        name="me"
-        :text="['hey, how are you?']"
-        sent
-      />
-      <q-chat-message
-        name="Jane"
-        :text="['doing fine, how r you?']"
+        v-for="message in messages"
+        :key="message.text"
+        :name="message.from"
+        :text="[message.text]"
+        :sent="message.from == 'me' ? true : false"
       />
     </div>
     <q-footer elevated>
@@ -40,7 +38,21 @@
 export default {
   data() {
     return {
-      newMessage: ''
+      newMessage: '',
+      messages: [
+        {
+          text: 'Hey babs how are you?',
+          from: 'me'
+        },
+        {
+          text: 'Good thanks babsy! How are you?',
+          from: 'them'
+        },
+        {
+          text: 'Im good babsy :)',
+          from: 'me'
+        }
+      ]
     }
   }
 }
