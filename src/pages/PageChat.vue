cc3b8bdff7f701be48979721e22175bd0357e6ba
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-04 14:45:12 +0800

    added sendMessage function and user offline banner

--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -1,34 +1,42 @@
 <template>
   <q-page class="flex column justify-end">
+    <q-banner class="bg-grey-4 text-center">
+      User is offline.
+    </q-banner>
     <div class="q-pa-md column col">
       <q-chat-message
         v-for="message in messages"
         :key="message.text"
         :name="message.from"
         :text="[message.text]"
-        :sent="message.from == 'me' ? true : false"
+        :sent="message.from === 'me' ? true : false"
       />
     </div>
     <q-footer elevated>
       <q-toolbar>
-        <q-input
-          v-model="newMessage"
-          bg-color="white"
-          class="full-width"
-          outlined
-          rounded
-          label="Message..."
-          dense>
+        <q-form
+          @submit="sendMessage"
+          class="full-width">
+          <q-input
+            v-model="newMessage"
+            bg-color="white"
+            class="full-width"
+            outlined
+            rounded
+            label="Message..."
+            dense>
 
-          <template v-slot:after>
-            <q-btn
-              round
-              dense
-              flat
-              color="white"
-              icon="send" />
-          </template>
-        </q-input>
+            <template v-slot:after>
+              <q-btn
+                round
+                dense
+                flat
+                @click="sendMessage"
+                color="white"
+                icon="send" />
+            </template>
+          </q-input>
+        </q-form>
       </q-toolbar>
     </q-footer>
   </q-page>
@@ -53,6 +61,14 @@ export default {
           from: 'me'
         }
       ]
+    }
+  },
+  methods: {
+    sendMessage() {
+      this.messages.push({
+        text: this.newMessage,
+        from: 'me'
+      })
     }
   }
 }
