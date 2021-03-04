ddf54661482f518fcad555a68ebcdcf841da1c29
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-04 15:00:36 +0800

    added component login-register

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -18,11 +18,11 @@
 
       <q-tab-panels v-model="tab" animated>
         <q-tab-panel name="login">
-          <h5>Login</h5>
+          <login-register />
         </q-tab-panel>
 
         <q-tab-panel name="register">
-          <h5>Register</h5>
+          <login-register />
         </q-tab-panel>
       </q-tab-panels>
     </q-card>
@@ -30,11 +30,15 @@
 </template>
 
 <script>
+
 export default {
   data () {
     return {
       tab: 'login'
     }
+  },
+  components: {
+    'login-register' : require('components/LoginRegister.vue').default
   }
 }
 </script>
