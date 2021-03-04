dfcfe706e4134e52ef74f69f4b598b9650d0f355
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-04 17:55:16 +0800

    added button for logout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,6 +16,7 @@
         </q-toolbar-title>
 
         <q-btn
+          v-if="!userDetails.userId"
           to="/auth"
           class="absolute-right q-pr-sm"
           icon="account_circle"
@@ -23,6 +24,18 @@
           flat
           dense
           label="Login" />
+        <q-btn
+          v-else
+          @click="logoutUser"
+          class="absolute-right q-pr-sm"
+          icon="account_circle"
+          no-caps
+          flat
+          dense
+        >
+          Logout<br>
+          {{ userDetails.name }}
+        </q-btn>
 
       </q-toolbar>
     </q-header>
@@ -33,8 +46,10 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
 export default {
   computed: {
+    ...mapState('store', ['userDetails']),
     title() {
       console.log(this.$route)
       let currentPath = this.$route.fullPath
@@ -42,6 +57,15 @@ export default {
       else if (currentPath === '/chat') return 'Chat'
       else if (currentPath === '/auth') return 'Login'
     }
+  },
+  methods: {
+    ...mapActions('store', ['logoutUser'])
   }
 }
 </script>
+
+<style lang="stylus">
+.q-toolbar
+  .q-btn
+    line-height: 1.2
+</style>
