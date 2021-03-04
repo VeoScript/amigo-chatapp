d2f38622005fbe74f6e95001f74a6f652e780e58
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-04 13:14:21 +0800

    designed message input, send button and chatbox

--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -1,11 +1,47 @@
 <template>
-  <q-page class="flex">
-    <h5>Chat Page</h5>
+  <q-page class="flex column justify-end">
+    <div class="q-pa-md column col">
+      <q-chat-message
+        name="me"
+        :text="['hey, how are you?']"
+        sent
+      />
+      <q-chat-message
+        name="Jane"
+        :text="['doing fine, how r you?']"
+      />
+    </div>
+    <q-footer elevated>
+      <q-toolbar>
+        <q-input
+          v-model="newMessage"
+          bg-color="white"
+          class="full-width"
+          outlined
+          rounded
+          label="Message..."
+          dense>
+
+          <template v-slot:after>
+            <q-btn
+              round
+              dense
+              flat
+              color="white"
+              icon="send" />
+          </template>
+        </q-input>
+      </q-toolbar>
+    </q-footer>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      newMessage: ''
+    }
+  }
 }
 </script>
