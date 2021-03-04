00cd2b71c65b0de14471698e5fdd9fbd4c374e97
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-04 20:02:42 +0800

    Update PageChat.vue

--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -43,33 +43,30 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
 export default {
   data() {
     return {
-      newMessage: '',
-      messages: [
-        {
-          text: 'Hey babs how are you?',
-          from: 'me'
-        },
-        {
-          text: 'Good thanks babsy! How are you?',
-          from: 'them'
-        },
-        {
-          text: 'Im good babsy :)',
-          from: 'me'
-        }
-      ]
+      newMessage: ''
     }
   },
+  computed: {
+    ...mapState('store', ['messages'])
+  },
   methods: {
+    ...mapActions('store', ['firebaseGetMessages', 'firebaseStopGettingMessages']),
     sendMessage() {
       this.messages.push({
         text: this.newMessage,
         from: 'me'
       })
     }
+  },
+  mounted() {
+    this.firebaseGetMessages(this.$route.params.otherUserId)
+  },
+  destroyed() {
+    this.firebaseStopGettingMessages()
   }
 }
 </script>
